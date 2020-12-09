c1012cb94018141b00f5d2e7a03ee2dd379464dd
Andrew Dauphinais <idollabschicago@gmail.com>
2020-12-09 17:23:49 -0600

    tour and peviews project complete

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -9,9 +9,13 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
   const fetchTours = async () => {
     setLoading(true);
-
     try {
       const response = await fetch(url);
       const tours = await response.json();
@@ -22,22 +26,31 @@ function App() {
       console.log(error);
     }
   };
-
   useEffect(() => {
     fetchTours();
   }, []);
-
   if (loading) {
     return (
       <main>
-        <Tours tours={tours} />
         <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
       </main>
     );
   }
   return (
     <main>
-      <Tours />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
