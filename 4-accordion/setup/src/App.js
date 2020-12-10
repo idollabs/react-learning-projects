16d06a240b388ff9cb72b1eeee66ce1d2be850aa
Andrew Dauphinais <idollabschicago@gmail.com>
2020-12-10 14:33:07 -0600

    note

--- 4-accordion/setup/src/App.js
+++ 4-accordion/setup/src/App.js
@@ -2,7 +2,19 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [question, setQuestion] = useState(data);
+
+  const testArr = [{ id: 1, name: 2, title: 3, year: 4 }];
+
+  const { id, name, title, year } = question;
+
+  console.log();
+
+  return question.map((props) => {
+    const { id, title, info } = props;
+    return <div key={id}>{title}</div>;
+  });
 }
 
 export default App;
+//note
